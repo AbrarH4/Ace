71c1521f9634c366503e6b74daf9c0b36d152330
Farbod Daneshjoo <f.daneshjoo12@gmail.com>
2026-07-09 11:56:08 +0330

    Update Ace.py

--- Ace.py
+++ Ace.py
@@ -4,7 +4,7 @@ Entry point for the application. Instantiates and starts the main
 CustomTkinter UI loop.
 """
 
-from UI import ACEUI
+from scripts.UI import ACEUI
 
 if __name__ == "__main__":
     app = ACEUI()
